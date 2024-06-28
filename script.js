923f9767dd2622aa3a654188e3bf9ccf4d418e0c
FiatUnoComEscadaDaClaro <127310216+FiatUnoComEscadaDaClaro@users.noreply.github.com>
2024-06-28 20:00:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+});
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-btn');
+const btnNext = document.getElementById('next-btn');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
